524440d3ef3c35ad71d1a137339eee1691c6f9e0
Wan-Derer <frd-ru@yandex.ru>
2021-12-08 14:01:31 +0300

    Lesson 92

--- src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.zaurtregulov.spring.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -13,15 +14,25 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
     auth.inMemoryAuthentication()
-      .withUser(userBuilder.username("Заур").password("zaur").roles("EMPLOYEE"))
-      .withUser(userBuilder.username("Елена").password("elena").roles("HR"))
-      .withUser(userBuilder.username("Иван").password("ivan").roles("MANAGER", "HR"))
+      .withUser(userBuilder.username("admin").password("admin").roles("ADMIN"))
+      .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE"))
+      .withUser(userBuilder.username("elena").password("elena").roles("HR"))
+      .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"))
     ;
 
 
   }
 
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
 
+    http.authorizeRequests()
+      .antMatchers("/").hasAnyRole("ADMIN", "EMPLOYEE", "HR", "MANAGER")
+      .antMatchers("/hr_info").hasAnyRole("ADMIN", "HR")
+      .antMatchers("/manager_info/**").hasAnyRole("ADMIN", "MANAGER")
+      .and().formLogin().permitAll();
+
+  }
 }
 
 
--- src/main/webapp/WEB-INF/view/view_for_all.jsp
+++ src/main/webapp/WEB-INF/view/view_for_all.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
@@ -8,13 +9,16 @@
 
 <h3>Информация для всех</h3>
 
+<security:authorize access="hasAnyRole('ADMIN', 'HR')">
 <input type="button" value="Зарплаты" onclick="window.location.href = 'hr_info'">
 Только для отдела кадров
+</security:authorize>
 
 <br><br>
+<security:authorize access="hasAnyRole('ADMIN', 'MANAGER')">
 <input type="button" value="Производительность" onclick="window.location.href = 'manager_info'">
 Только для руководителей
-
+</security:authorize>
 
 </body>
 </html>
